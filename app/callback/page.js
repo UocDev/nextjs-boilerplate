189f4844d24b7dc99102e4b9fcdbd44ba6428294
Uoc Developer <vanssenlingga20@gmail.com>
2025-05-24 08:15:47 +0800

    Update page.js

--- app/callback/page.js
+++ app/callback/page.js
@@ -1,32 +1,10 @@
-'use client';
+import { Suspense } from 'react';
+import CallbackClient from './CallbackClient';
 
-import { Suspense, useEffect } from 'react';
-
-function OAuthHandler() {
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get('code');
-
-    if (code) {
-      console.log('OAuth2 code:', code);
-      setTimeout(() => {
-        window.location.href = 'https://discord.gg/wQmKyRm5rx';
-      }, 3000);
-    }
-  }, []);
-
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Authorization Complete</h1>
-      <p>Redirecting to our Discord server...</p>
-    </div>
-  );
-}
-
-export default function Page() {
+export default function CallbackPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <OAuthHandler />
+      <CallbackClient />
     </Suspense>
   );
 }
